1eeef8700e3f72bb372277615a6e77927bf59eab
Miguel Angel Morales <mmorales21500@live.com>
2021-02-02 21:13:09 -0500

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,15 @@
+# lib/introduction.rb
+
+def introduction
+end 
+# lib/introduction.rb
+
+def introduction(name)
+end 
+
+# lib/introduction.rb
+
+def introduction(name)
+    puts "Hi, my name is #{Miguel} and i am learing to program in #{ruby}."
+    
+  end 
